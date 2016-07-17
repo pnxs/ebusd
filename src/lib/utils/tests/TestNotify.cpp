1e836a259abb1e42f5861d2a1f11aa18518fafad
Thomas Schätzlein <thomas@pnxs.de>
2016-07-17 10:29:07 +0200

    add unittest for Notify

--- src/lib/utils/tests/TestNotify.cpp
+++ src/lib/utils/tests/TestNotify.cpp
@@ -1,6 +1,25 @@
 #include "gtest/gtest.h"
+#include "notify.h"
 
 TEST(TestNotify, basicTest)
 {
-    ASSERT_TRUE(false);
+    Notify notify;
+
+    ASSERT_TRUE(notify.notifyFD() > 0);
+
+    int fd = notify.notifyFD();
+    int ret = fcntl(fd, F_SETFL, O_NONBLOCK);
+    ASSERT_EQ(ret, 0);
+
+    char b;
+    auto rc = read(fd, &b, sizeof(b));
+
+    ASSERT_EQ(rc, -1);
+    ASSERT_EQ(errno, EAGAIN);
+
+    notify.notify();
+
+    rc = read(fd, &b, sizeof(b));
+
+    ASSERT_EQ(rc, 1);
 }
